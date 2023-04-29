f3154512a9e182ef23e1938de577726df099dd8b
BLESS <2978103904@qq.com>
2023-04-30 00:33:13 +0800

    modified model for the single agent

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 # coding=utf-8
 
-import hashlib
-import logging
-import time
 import tqdm
-
+import gymnasium as gym
 import tensorflow as tf
-
+# from envs.func import SecFunc
 from envs.micro_grid import MicroGrid
 from newa2c import Agent
+import random
 
 if __name__ == "__main__":
-
-    agent = Agent(128, 0, -200, 200, 100, 400, 0, 500, 0.0035, 0.0025, 8, 300)
-    env = MicroGrid()
-    env.define_observation_space('./envs/load.csv', './envs/prize.csv', './envs/pv.csv')
-
-    first_state = tf.expand_dims(env.reset(), 0)
-    with tqdm.trange(20) as t:
+    # turn = 0
+    writer = tf.summary.create_file_writer("./log")
+    agent = Agent(128, 0, -500, 500, 0, 1000, 0, 1000, 0.0035, 0.00025, 0.0025, 8, 0.05)
+    env = MicroGrid(writer)
+    env.define_observation_space('./envs/prize.csv', './envs/load.csv', './envs/pv.csv')
+    with tqdm.trange(10000) as t:
+        # env.render() random.randint(0, 1369)
         for i in t:
-            if i != 0:
-                env.turn()
-            for j in range(env.columns):
-                next_state, reward = agent.multi_object(first_state, env)
-                # print(reward)
-                first_state = next_state
-            t.set_description(f'Episode {i}')
-    first_state = tf.expand_dims(env.reset(), 0)
-    _, reward = agent.multi_object(first_state, env)
-    print("test reward", reward)
+            first_state = env.reset(0)
+            first_state = tf.expand_dims(first_state, 0)
+            reward = agent.multi_object(first_state, env, writer, i)
+            t.set_description(f'Episode {i} reward {reward}')
+            with writer.as_default():
+                tf.summary.scalar('all_reward', float(reward), step=i)
+                tf.summary.flush()
+        writer.close()
+
